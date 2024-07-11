906d55a44ca23770689b71fb77922d5ab56fd623
jonnyjackson26 <jrsjackson26@gmail.com>
2024-07-10 20:08:46 -0600

    grid/list view switcher for books on homepage

--- src/pages/Home/_Home.jsx
+++ src/pages/Home/_Home.jsx
@@ -1,18 +1,23 @@
-import { Link } from "react-router-dom"
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
-import React, { useState, useEffect, useContext } from 'react';
 import books from '../../../public/data/books.js';
 import NavBar from "../../components/NavBar/NavBar.jsx";
-import myData from "../../../public/data/_languages.js"
+import myData from "../../../public/data/_languages.js";
 import DocumentTitle from "../../components/DocumentTitle.jsx";
+import GridListViewSwitcher from "../../components/GridListViewSwitcher/GridListViewSwitcher.jsx";
 
-import { Context } from "../../main.jsx"; //can make custom hook
-
+import { Context } from "../../main.jsx";
 
 export function Home() {
     const [language, setLanguage] = useContext(Context);
+    const [isGridView, setIsGridView] = useState(true); // State for view mode
 
     DocumentTitle(myData[language]["bookOfMormon"]);
+
+    const handleViewChange = (isGrid) => {
+        setIsGridView(isGrid); // Update view mode state
+    };
 
     return (
         <>
@@ -21,24 +26,17 @@ export function Home() {
                 {myData[language]["book-of-mormon"]} {/*The Book of Mormon: Another Testament of Jesus Christ*/}
             </h1>
 
-            <div className="book-container">
+            <GridListViewSwitcher onViewChange={handleViewChange} />
+
+            <div className={isGridView ? "book-container-grid" : "book-container-list"}>
                 {books.map((book) => (
-                    <Link className="book" key={book.urlName} to={`/${book.urlName}`}>
-                        {myData[language][book.urlName]} {/* Worte von Mormon or Words of Mormon */}
+                    <Link className={isGridView ? "book-grid" : "book-list"} key={book.urlName} to={`/${book.urlName}`}>
+                        {myData[language][book.urlName]}
                     </Link>
                 ))}
             </div>
         </>
-    )
+    );
 }
 
-
-/*
-TODO:
- - put nav bar on bottom for mobile
- - fix language dropdown
-
- long term:
- - sell books
- - add quotes in a json file
-*/
+export default Home;
